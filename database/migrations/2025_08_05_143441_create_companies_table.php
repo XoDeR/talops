0f5a9266c77cec1454b219dc9eb70af35d599fbb
XoDeR <xoder.vs@gmail.com>
2025-08-05 18:34:17 +0200

    migrations

--- database/migrations/2025_08_05_143441_create_companies_table.php
+++ database/migrations/2025_08_05_143441_create_companies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique()->index();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
