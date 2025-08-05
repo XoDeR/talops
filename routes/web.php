c2a9895f916cc7401709f1308303ea033be5d50b
XoDeR <xoder.vs@gmail.com>
2025-08-05 16:33:35 +0200

    default admin login completed

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return redirect('dashboard');
+    //return Inertia::render('Welcome');
 })->name('home');
 
 Route::get('dashboard', function () {
