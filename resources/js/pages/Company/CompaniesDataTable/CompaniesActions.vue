00951e36c938b23a064c2af5232d27b4f973b3f3
XoDeR <xoder.vs@gmail.com>
2025-08-06 19:39:39 +0200

    companies actions

--- resources/js/pages/Company/CompaniesDataTable/CompaniesActions.vue
+++ resources/js/pages/Company/CompaniesDataTable/CompaniesActions.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { MoreHorizontal } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { router } from '@inertiajs/vue3'
 
 defineProps<{
   companyDisplay: {
@@ -11,14 +12,31 @@ defineProps<{
 
 function view(uuid: string) {
   console.log("view")
+  router.visit(`/companies/${uuid}`, {
+    method: 'get',
+  })
 }
 
 function edit(uuid: string) {
   console.log("edit")
+  router.visit(`/companies/${uuid}/edit`, {
+    method: 'get',
+  })
 }
 
 function deleteItem(uuid: string) {
   console.log("delete")
+  if (confirm("Are you sure you want to delete this company?")) {
+    router.delete(`/companies/${uuid}`, {
+      preserveScroll: true,
+      preserveState: true,
+      onSuccess: () => {
+        console.log("Company deleted")
+      },
+    })
+  } else {
+    console.log("Deletion cancelled")
+  }
 }
 </script>
 
