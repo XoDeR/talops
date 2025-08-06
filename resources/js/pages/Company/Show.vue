91d7d076b70aa9e1452c95ee6d2e55e783f7e77c
XoDeR <xoder.vs@gmail.com>
2025-08-06 19:39:17 +0200

    view/create/edit empty pages with default layout for companies and employees

---
+++ resources/js/pages/Company/Show.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+  {
+    title: 'Companies',
+    href: '/companies',
+  },
+];
+</script>
+
+<template>
+
+  <Head title="Company" />
+  <AppLayout :breadcrumbs="breadcrumbs">
+    Company
+  </AppLayout>
+</template>
---
+++ resources/js/pages/Employee/Edit.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+  {
+    title: 'Employees',
+    href: '/employees',
+  },
+];
+</script>
+
+<template>
+
+  <Head title="Employee" />
+  <AppLayout :breadcrumbs="breadcrumbs">
+    Employee
+  </AppLayout>
+</template>
